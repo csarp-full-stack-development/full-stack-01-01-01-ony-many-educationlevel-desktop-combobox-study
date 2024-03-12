1285fdae931fefaa853cb6871639591bb98674a9
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-03-12 14:37:08 +0100

    Diák adatok lekérése a kiterjesztett adatokkal (api/Student/included)

--- Kreta.HttpService/Services/StudentService.cs
+++ Kreta.HttpService/Services/StudentService.cs
@@ -11,5 +11,27 @@ namespace Kreta.HttpService.Services
         public StudentService(IHttpClientFactory? httpClientFactory, StudentAssambler studentAssambler) : base(httpClientFactory, studentAssambler)
         {
         }
+
+        public async Task<List<Student>> SelectAllIncludedAsync()
+        {
+            if (_httpClient is not null)
+            {
+                try
+                {
+
+                    List<StudentDto>? resultDto = await _httpClient.GetFromJsonAsync<List<StudentDto>>($"api/Student/included");
+                    if (resultDto is not null)
+                    {
+                        List<Student> result = resultDto.Select(entity => _assambler.ToModel(entity)).ToList();
+                        return result;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+            return new List<Student>();
+        }
     }
 }
