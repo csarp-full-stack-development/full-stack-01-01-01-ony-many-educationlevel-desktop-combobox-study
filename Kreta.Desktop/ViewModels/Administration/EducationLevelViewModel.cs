94c99047af770e2bfc57774b67cd91f10fcbf60f
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-03-13 10:28:51 +0100

    ListBox property-k a ViewModel-ben

--- Kreta.Desktop/ViewModels/Administration/EducationLevelViewModel.cs
+++ Kreta.Desktop/ViewModels/Administration/EducationLevelViewModel.cs
@@ -1,9 +1,18 @@
-﻿using Kreta.Desktop.ViewModels.Base;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Kreta.Desktop.ViewModels.Base;
+using Kreta.Shared.Models;
+using System.Collections.ObjectModel;
 
 namespace Kreta.Desktop.ViewModels.Administration
 {
     public partial class EducationLevelViewModel : BaseViewModel
     {
         public string Title { get; set; } = "Tanulmányi szint kezelése";
+
+        [ObservableProperty]
+        private ObservableCollection<EducationLevel> _educationLevels=new();
+
+        [ObservableProperty]
+        private EducationLevel _selectedEducationLevel = new();
     }
 }
